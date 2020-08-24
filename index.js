4326d9db5216a0fa6dc3bbd7e86ccde28fb700bd
Hector Fuentes <hector.fuentes@imagemaker.com>
2020-08-24 00:02:30 -0400

    resolvers example with static data

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ApolloServer } = require('apollo-server-express')
-const typeDefs = require('./schema')
+const { typeDefs, resolvers} = require('./schema')
 
-const server = new ApolloServer({ typeDefs })
+const server = new ApolloServer({ typeDefs, resolvers })
 const app = express()
 server.applyMiddleware({ app })
 
--- schema.js
+++ schema.js
@@ -1,13 +1,13 @@
 const { gql } = require('apollo-server-express')
 
-module.exports = gql`
+module.exports.typeDefs = gql`
 
     type Curso {
         id: ID!
         titulo: String!
         descripcion: String!
         profesor: Profesor
-        rating: Float
+        rating: Float @deprecated(reason: "No creemos más en los puntajes de cursos!")
         comentarios: [Comentario]
     }
 
@@ -37,3 +37,19 @@ module.exports = gql`
         profesor(id: Int): Profesor
     }
 `
+
+module.exports.resolvers = {
+    Query: {
+        cursos: () => [{
+            id: 1, titulo: "Curso Ejemplo", descripcion: "ASD123"
+        }],
+        profesores: () => {
+            return new Promise((resolve, reject) => {
+                return resolve([{
+                    id: 99,
+                    nombre: 'Hector'
+                }])
+            })
+        }
+    }
+}
